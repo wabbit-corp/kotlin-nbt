fe18d0d0d0680c50bc1926f3a2d509bc0a5013e9
Sir Wabbit <559086+sir-wabbit@users.noreply.github.com>
2026-03-08 23:43:51 -0400

    chore: refresh legal docs and build config
    Semver Impact: NONE

--- build.gradle.kts
+++ build.gradle.kts
@@ -92,11 +92,13 @@ dokka {
     }
     dokkaSourceSets.main {
         // includes.from("README.md")
+
         sourceLink {
             localDirectory.set(file("src/main/kotlin"))
-            remoteUrl("https://example.com/src")
+            remoteUrl("https://github.com/wabbit-corp/kotlin-nbt/tree/master/src/main/kotlin")
             remoteLineSuffix.set("#L")
         }
+
     }
     pluginsConfiguration.html {
         // customStyleSheets.from("styles.css")
